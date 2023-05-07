0b27df448058035b33f39e90ff73282367a2814f
hadar gueta <hadart20@gmail.com>
2023-05-07 22:49:45 +0300

    add auth and setup

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+  return <div >
+    <h1>this is Home page</h1>
+    <div>
+      {!!user ? (
+        <>
+          <div>
+            <Image src={user.picture} alt={user.name} height={50} width={50} />
+          <div>{user.email}</div>
+          </div>
+          <Link href="/api/auth/logout">Logout</Link>
+        </>
+      ) : (
+        <Link href="/api/auth/login">Login</Link>
+      )}
+    </div>
+  </div>;
 }
